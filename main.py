36e322bb2c47267c738bae25daf8226fd2684e33
AKA717 <alwinaji717@gmail.com>
2023-10-23 14:20:17 +0530

    created news ListView added

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+import flet as ft
 from scrape import fetch_news
 
-if __name__ == "__main__":
-    news_list = fetch_news()
-    print(news_list)
+def main(page):
+    page.title = "BestOfHackerNews"
+
+    page.add(ft.Text(
+            "HackerNews",
+            size=20,
+            color=ft.colors.TEAL_ACCENT_200,
+            weight=ft.FontWeight.BOLD,
+        ))
+    
+    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+
+    news  = fetch_news()
+    
+    for n in news:
+        lv.controls.append(
+            ft.Card(
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.ALBUM),
+                            title=ft.Text(n['title']),
+                            subtitle=ft.Text(
+                                n['url']
+                            )
+                        )
+                    ]
+                ),
+                expand=True,
+                padding=10,
+            )
+        )
+        )
+        
+    page.add(lv)
+
+ft.app(target=main)
